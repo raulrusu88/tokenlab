358db05cfb80d3755db0bb1313638d88e0222880
Raul Rusu <raulrusu46@gmail.com>
2021-12-16 14:18:11 +0200

    feat: tailwind style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@ const defaultThemes = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./core/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -14,6 +11,12 @@ module.exports = {
         text: "#9CB3C9",
         positive: "#69BFAF",
         negative: "#F1A1A1",
+      },
+      width: {
+        "8xl": "87.5rem", // 1400px
+      },
+      gridTemplateColumns: {
+        tableRow: "50px 230px repeat(5, 130px) 200px",
       },
       fontFamily: {
         pro: ["Source Sans Pro", ...defaultThemes.fontFamily.sans],
