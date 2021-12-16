14b48a157c94a3700fb61be6a9865aabac090b73
Raul Rusu <raulrusu46@gmail.com>
2021-12-16 14:17:37 +0200

    feat: styled layout

--- core/layouts/Layout.tsx
+++ core/layouts/Layout.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent, ReactNode } from "react";
+import { Navigation } from "../components/Navigation/Navigation";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-primary">
-      <div className="w-10 bg-red-500">{children}</div>
+    <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
+      <Navigation />
+      <main>{children}</main>
     </div>
   );
 };
