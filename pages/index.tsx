de8fff6ef3829e83e21aee41d365026e410b312c
Raul Rusu <raulrusu46@gmail.com>
2021-12-16 14:19:14 +0200

    feat: added coinview to the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import { CoinsView } from "../core/layouts/CoinsView/CoinsView";
 import { Layout } from "../core/layouts/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <main className="">
+      <CoinsView>
         <h1 className="text-blue-400">Welcome to Tokenlab</h1>
-      </main>
+      </CoinsView>
     </Layout>
   );
 }
