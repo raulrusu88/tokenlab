41bb19348f457e0b4c61ffe086e57d1f7a1bb1bd
Raul Rusu <raulrusu46@gmail.com>
2021-12-18 00:01:56 +0200

    feat: auth0 provider and dependecy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import "../styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const isSSR = typeof window !== "undefined";
+
+  const auth0_settings = {
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT,
+  };
+
+  return (
+    <Auth0Provider
+      {...auth0_settings}
+      redirectUri={isSSR && window.location.origin}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
 export default MyApp;
