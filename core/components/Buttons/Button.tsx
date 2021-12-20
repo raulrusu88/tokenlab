4926bf481212ac2b8cd7d7f67975e993bf0d9a27
Raul Rusu <raulrusu46@gmail.com>
2021-12-20 19:07:16 +0200

    update: added some props and styles for the button - still buggy on hover

--- core/components/Buttons/Button.tsx
+++ core/components/Buttons/Button.tsx
@@ -1,13 +1,19 @@
+import { MouseEventHandler } from "react";
 import Link from "next/link";
 
 interface IButton {
   text: string;
+  href?: string;
+  onClick?: () => void;
 }
 
 export const Button = (props: IButton) => {
   return (
-    <Link href="#">
-      <a className="h-[34px] text-lg px-4 flex items-center justify-center w-max transition hover:border-secondary hover:border-2">
+    <Link href={props.href || "#"}>
+      <a
+        onClick={props.onClick}
+        className="h-[34px] text-lg px-4 flex items-center justify-center w-max transition hover:border-secondary hover:border-2"
+      >
         <span className="font-semibold">{props.text}</span>
       </a>
     </Link>
