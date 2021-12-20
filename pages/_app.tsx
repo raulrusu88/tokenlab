ac618b5d4db1a924f6c1a8a56e80d689a8f165dd
Raul Rusu <raulrusu46@gmail.com>
2021-12-20 19:09:10 +0200

    fix: via the nextjs env I updated the auth0 envs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 function MyApp({ Component, pageProps }) {
   const isSSR = typeof window !== "undefined";
 
-  const auth0_settings = {
-    domain: process.env.AUTH0_DOMAIN,
-    clientId: process.env.AUTH0_CLIENT,
-  };
-
   return (
     <Auth0Provider
-      {...auth0_settings}
+      domain={process.env.auth0_domain}
+      clientId={process.env.auth0_client}
       redirectUri={isSSR && window.location.origin}
     >
       <Component {...pageProps} />
