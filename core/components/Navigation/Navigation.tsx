0fc223565ab4ed35017ac57ba988054ed98f6abf
Raul Rusu <raulrusu46@gmail.com>
2021-12-20 19:08:47 +0200

    feat: auth0 creates an account and saves it into the db

--- core/components/Navigation/Navigation.tsx
+++ core/components/Navigation/Navigation.tsx
@@ -1,7 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "../Buttons/Button";
 import { Logo } from "./Logo";
 
 export const Navigation = () => {
+  const { loginWithPopup, user, isLoading } = useAuth0();
+
+  const handleSignup = async () => {
+    loginWithPopup();
+
+    const data = {
+      auth0_id: user.sub.replace("|", " ").split(" ")[1],
+    };
+
+    // TODO: Move eveyrthing that is server/fetch related into a utils file and just call it from there.
+    const res = await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    });
+    return res.json();
+  };
+
   return (
     <nav className="w-full h-16 flex items-center text-text">
       <div className="grid grid-cols-3 grid-rows-none w-full">
@@ -10,7 +37,7 @@ export const Navigation = () => {
           <Logo />
         </div>
         <div className="flex justify-center flex-row">
-          <Button text="Sign Up" />
+          <Button text="Sign Up" onClick={handleSignup} />
           <Button text="Log In" />
         </div>
       </div>
