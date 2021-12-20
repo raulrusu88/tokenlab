2eafea73b7228fffd0012f004436d3ba310aada5
Raul Rusu <raulrusu46@gmail.com>
2021-12-20 19:06:30 +0200

    feat: nextjs env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    auth0_domain: process.env.AUTH0_DOMAIN,
+    auth0_client: process.env.AUTH0_CLIENT,
+    auth0_secret: process.env.AUTH0_SECRET,
+  },
+};
