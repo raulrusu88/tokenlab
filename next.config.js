f1372253f49962da8225f0b835c10ffb1a63d9e3
Raul Rusu <raulrusu46@gmail.com>
2021-12-23 22:05:42 +0200

    update: coinmarketcap envs

--- next.config.js
+++ next.config.js
@@ -4,5 +4,7 @@ module.exports = {
     auth0_domain: process.env.AUTH0_DOMAIN,
     auth0_client: process.env.AUTH0_CLIENT,
     auth0_secret: process.env.AUTH0_SECRET,
+    cmc_url: process.env.CMC_URL,
+    cmc_api: process.env.CMC_API_KEY,
   },
 };
