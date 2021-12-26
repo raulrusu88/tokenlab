414eaa0ac016b947632c5184ee3c26b3dbad8980
Raul Rusu <raulrusu46@gmail.com>
2021-12-26 15:53:16 +0200

    fix: cleanup

--- core/components/Navigation/Navigation.tsx
+++ core/components/Navigation/Navigation.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "../Buttons/Button";
 import { Logo } from "./Logo";
