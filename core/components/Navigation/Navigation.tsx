b8d8515579eeedb6e9e5c0c3477600fec396e2c7
hazelnutx <raulrusu46@gmail.com>
2021-12-27 20:17:00 +0200

    update: removed the onMount useeffect to check if there is a user, as I know it works (for now) and added a link to the logo back to the / path

--- core/components/Navigation/Navigation.tsx
+++ core/components/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { Button } from "../Buttons/Button";
 import { Logo } from "./Logo";
 import { useAuth } from "../../context/AuthContext";
@@ -6,21 +7,23 @@ import { useAuth } from "../../context/AuthContext";
 export const Navigation = () => {
   const { signInWithPopup, currentUser, isAuthenticated, logOut } = useAuth();
 
-  React.useEffect(() => {
-    if (isAuthenticated) console.log(currentUser);
-    else "not logged in";
-  }, [currentUser, isAuthenticated]);
-
   return (
     <nav className="w-full h-16 flex items-center text-text">
       <div className="grid grid-cols-3 grid-rows-none w-full">
         <div />
         <div className="flex items-center justify-center flex-row">
-          <Logo />
+          <Link href="/" passHref>
+            <a className="w-max flex items-center">
+              <Logo />
+            </a>
+          </Link>
         </div>
         <div className="flex justify-center flex-row">
           {isAuthenticated ? (
-            <Button text="Log out" onClick={logOut} />
+            <>
+              <Button text="Dashboard" href="/dashboard" />
+              <Button text="Log out" onClick={logOut} />
+            </>
           ) : (
             <>
               <Button text="Sign In" onClick={signInWithPopup} />
