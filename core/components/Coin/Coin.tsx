bbc01401b4ce824e194cd16456b6152b30a7639f
hazelnutx <raulrusu46@gmail.com>
2021-12-29 15:37:59 +0200

    feat: new coin component test

---
+++ core/components/Coin/Coin.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import { useAuth } from "../../context/AuthContext";
+import { Input } from "../Forms/Input";
+import { Select } from "../Forms/Select";
+
+interface Props {
+  index: number;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_percentage_24h: number;
+}
+
+/**
+ * @param symbol string
+ * @param name string
+ * @param image string
+ * @param current_price number
+ * @param market_cap number
+ * @param high_24h number
+ * @param low_24h number
+ * @param price_change_percentage_24h number
+ */
+export const Coin = (props: Props) => {
+  const {
+    index,
+    symbol,
+    name,
+    image,
+    current_price,
+    market_cap,
+    high_24h,
+    low_24h,
+    price_change_percentage_24h,
+  } = props;
+  const { isAuthenticated } = useAuth();
+  return (
+    <div className="flex h-14 w-full  items-center justify-between">
+      <span className="text-text/50 mr-3">{index}</span>
+      <div className="flex w-36">
+        <Image src={image} alt={name} layout="fixed" width="24" height="24" />
+        <p className="text-text font-semibold ml-2">{name}</p>
+        <p className="text-text/50 font-base ml-1">{symbol.toUpperCase()}</p>
+      </div>
+      <div className="mx-4 ">
+        <p className="text-text font-semibold ">{current_price}</p>
+      </div>
+      <div className="mx-4">
+        <p className="text-text font-semibold ">{market_cap}</p>
+      </div>
+      <div className="mx-4">
+        <p className="text-text font-semibold ">{high_24h}</p>
+      </div>
+      <div className="mx-4">
+        <p className="text-text font-semibold ">{low_24h}</p>
+      </div>
+      <div className="mx-4">
+        <p className="text-text font-semibold ">
+          <span
+            className={
+              price_change_percentage_24h > 0
+                ? "text-positive"
+                : "text-negative"
+            }
+          >
+            {price_change_percentage_24h}
+          </span>
+        </p>
+      </div>
+      <div className="w-28">
+        <Select selectValue={(n) => console.log(n)} />
+      </div>
+      <div className="w-28">
+        <Input />
+      </div>
+    </div>
+  );
+};
--- core/layouts/CoinsView/CoinsView.tsx
+++ core/layouts/CoinsView/CoinsView.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { Coin } from "../../components/Coin/Coin";
 import { Input } from "../../components/Forms/Input";
 import { Select } from "../../components/Forms/Select";
 import { Line } from "../../components/Line";
@@ -27,6 +29,8 @@ interface CoinProps {
 export const CoinsView = ({ data }: CoinProps) => {
   const { isAuthenticated } = useAuth();
 
+  const [coins, setCoins] = useState([]);
+
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -52,7 +56,7 @@ export const CoinsView = ({ data }: CoinProps) => {
     if (tier) {
       numberFixed = (number / tier.value).toFixed(2);
     }
-    return `${numberFixed}${tier?.symbol}`;
+    return `$${numberFixed}${tier?.symbol}`;
   };
 
   const price = (x: number) => formatter.format(parseFloat(x.toString()));
@@ -66,61 +70,44 @@ export const CoinsView = ({ data }: CoinProps) => {
   // TODO: Select value does not work properly, as it should pass the data from the row it is used
   // and when you click the button to add the trade, it should forward the selected value.
   // as of now, it passes only 1, as it is from the Select component initial value
+  const fetchData = useCallback(async () => {
+    await axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="mt-12">
       <CV.Layout>
         <CV.Header>
-          <CV.Title text="Market Trends" />
+          <CV.Title text="Market" />
           <CV.Search />
         </CV.Header>
         <Line />
-        <CV.Table>
-          <thead>
-            <CV.TableRow>
-              <CV.TableHead text="#" />
-              <CV.TableHead text="Name" />
-              <CV.TableHead text="Price" />
-              <CV.TableHead text="24H Change(%)" />
-              <CV.TableHead text="24H Volume(USD)" />
-              {isAuthenticated && (
-                <>
-                  <CV.TableHead text="Leverage" />
-                  <CV.TableHead text="Amount(USD)" />
-                  <CV.TableHead text="Long/Short" />{" "}
-                </>
+        <div className="flex flex-col">
+          {coins.map((coin, index) => (
+            <Coin
+              key={coin.id}
+              {...coin}
+              index={index + 1}
+              current_price={price(coin.current_price)}
+              market_cap={volume24h(coin.market_cap)}
+              high_24h={price(coin.high_24h)}
+              low_24h={price(coin.low_24h)}
+              price_change_percentage_24h={coin.price_change_percentage_24h.toFixed(
+                2
               )}
-            </CV.TableRow>
-          </thead>
-          <tbody>
-            {data.map((d) => (
-              <CV.TableRow key={d.id}>
-                <CV.TableData>{d.cmc_rank}</CV.TableData>
-                <CV.TableData>{d.name}</CV.TableData>
-                <CV.TableData>{price(d.quote.USD.price)}</CV.TableData>
-                <CV.TableData>{d.quote.USD.volume_change_24h}</CV.TableData>
-                <CV.TableData>{volume24h(d.quote.USD.volume_24h)}</CV.TableData>
-                {isAuthenticated && (
-                  <>
-                    <CV.TableData>
-                      <Select selectValue={(n) => handleSelectValue(n)} />
-                    </CV.TableData>
-                    <CV.TableData>
-                      <Input />
-                    </CV.TableData>
-                    <CV.TableData>
-                      <CoinButton
-                        data={d}
-                        id={d.id}
-                        selectValue={selectValue}
-                      />
-                    </CV.TableData>
-                  </>
-                )}
-              </CV.TableRow>
-            ))}
-          </tbody>
-        </CV.Table>
+            />
+          ))}
+        </div>
       </CV.Layout>
     </div>
   );
@@ -148,3 +135,53 @@ const CoinButton = ({
     </>
   );
 };
+
+// TODO: Change it to a table
+// const Table = () => (
+//   <CV.Table>
+//           <thead>
+//             <CV.TableRow>
+//               <CV.TableHead text="#" />
+//               <CV.TableHead text="Name" />
+//               <CV.TableHead text="Price" />
+//               <CV.TableHead text="24H Change(%)" />
+//               <CV.TableHead text="24H Volume(USD)" />
+//               {isAuthenticated && (
+//                 <>
+//                   <CV.TableHead text="Leverage" />
+//                   <CV.TableHead text="Amount(USD)" />
+//                   <CV.TableHead text="Long/Short" />{" "}
+//                 </>
+//               )}
+//             </CV.TableRow>
+//           </thead>
+//           <tbody>
+//             {data.map((d) => (
+//               <CV.TableRow key={d.id}>
+//                 <CV.TableData>{d.cmc_rank}</CV.TableData>
+//                 <CV.TableData>{d.name}</CV.TableData>
+//                 <CV.TableData>{price(d.quote.USD.price)}</CV.TableData>
+//                 <CV.TableData>{d.quote.USD.volume_change_24h}</CV.TableData>
+//                 <CV.TableData>{volume24h(d.quote.USD.volume_24h)}</CV.TableData>
+//                 {isAuthenticated && (
+//                   <>
+//                     <CV.TableData>
+//                       <Select selectValue={(n) => handleSelectValue(n)} />
+//                     </CV.TableData>
+//                     <CV.TableData>
+//                       <Input />
+//                     </CV.TableData>
+//                     <CV.TableData>
+//                       <CoinButton
+//                         data={d}
+//                         id={d.id}
+//                         selectValue={selectValue}
+//                       />
+//                     </CV.TableData>
+//                   </>
+//                 )}
+//               </CV.TableRow>
+//             ))}
+//           </tbody>
+//         </CV.Table>
+// )
