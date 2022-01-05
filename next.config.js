372804306bf4579b99c57b9fdaa21d62f29df044
Raul Rusu <raulrusu46@gmail.com>
2022-01-05 15:12:35 +0200

    feat: firebase setup

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ module.exports = {
     domains: ["assets.coingecko.com"],
   },
   env: {
-    auth0_domain: process.env.AUTH0_DOMAIN,
-    auth0_client: process.env.AUTH0_CLIENT,
-    auth0_secret: process.env.AUTH0_SECRET,
-    cmc_url: process.env.CMC_URL,
-    cmc_api: process.env.CMC_API_KEY,
+    firebase_api_key: process.env.FIREBASE_API_KEY,
+    firebase_auth_domain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebase_project_id: process.env.FIREBASE_PROJECT_ID,
+    firebase_storage_bucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebase_messaging_sender_id: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebase_app_id: process.env.FIREBASE_APP_ID,
   },
 };
