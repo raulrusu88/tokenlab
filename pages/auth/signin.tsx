4f18c370d1d6cd891dae0762fc4c6b3f61cf1d20
Raul Rusu <raulrusu46@gmail.com>
2022-01-05 15:48:02 +0200

    update: auth pages now have some style

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@context/AuthContext";
 import { Layout } from "@layouts/Layout";
 import { FormEvent, useRef } from "react";
+import { LockClosedIcon } from "@heroicons/react/solid";
 
 export default function SignIn() {
   const { signInWithEmailAndPass } = useAuth();
@@ -18,15 +19,92 @@ export default function SignIn() {
 
   return (
     <Layout>
-      <form
-        onSubmit={(e) =>
-          handleSubmit(emailRef.current.value, passwordRef.current.value, e)
-        }
-      >
-        <input placeholder="email" ref={emailRef} type="email" />
-        <input placeholder="password" ref={passwordRef} type="password" />
-        <button type="submit">sign up</button>
-      </form>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div className="h-20" />
+          <form
+            className="mt-8 space-y-6"
+            onSubmit={(e) =>
+              handleSubmit(emailRef.current.value, passwordRef.current.value, e)
+            }
+          >
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                  ref={emailRef}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  ref={passwordRef}
+                />
+              </div>
+            </div>
+
+            {/* <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div> */}
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
